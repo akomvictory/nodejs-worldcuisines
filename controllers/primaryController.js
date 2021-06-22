da55ef10f92e7efd15743947eb81cc6c956d40af
Victory Akom <hellonetguba@gmail.com>
2021-06-22 22:20:35 +0100

    completed uruguayan routes

--- controllers/primaryController.js
+++ controllers/primaryController.js
@@ -13,21 +13,103 @@ const index = (req, res) => {
    res.render('index1', { title: 'Home page' });
   
   }
-
-  const asian_countries = (req, res) => {
-    res.render('asian/countries', { title: 'Asian cuisines' });
+  
+  const european_countries = (req, res) => {
+    res.render('europe/countries', { title: 'European cuisines' });
+   }
    
+   const spanish_cuisines = (req, res) => {
+    res.render('europe/spanish-cuisines', { title: 'Spanish cuisines' });
+   }
+
+   const esp_gazpaco = (req, res) => {
+    res.render('europe/esp-gazpaco', { title: 'Gazpaco cuisine' });
+   }
+
+   const esp_paella = (req, res) => {
+    res.render('europe/esp-paella', { title: 'Paella cuisine' });
+   }
+   
+   const esp_gambas = (req, res) => {
+    res.render('europe/esp-gambas', { title: 'Gambas Al aljillo cuisine' });
+   }
+
+   const fr_cuisines = (req, res) => {
+    res.render('europe/fr-cuisines', { title: 'french  cuisine' });
+   }
+
+   const fr_bouillabaisse = (req, res) => {
+    res.render('europe/fr-bouillabaisse', { title: 'Pozole cuisine' });
+   }
+
+   const fr_monsieur = (req, res) => {
+    res.render('europe/fr-monsieur', { title: 'Croque monsieur cuisine' });
+   }
+
+   const fr_lorraine = (req, res) => {
+    res.render('europe/fr-lorraine', { title: 'Lorraine cuisine' });
+   }
+
+   const itl_cuisines = (req, res) => {
+    res.render('europe/itl-cuisines', { title: 'Italian cuisine' });
+   }
+
+   
+   const mexican_cuisines = (req, res) => {
+    res.render('americas/mx-cuisines', { title: 'Mexican cuisine' });
+   }
+
+   const mx_pozole = (req, res) => {
+    res.render('americas/mx-pozole', { title: 'Pozole cuisine' });
+   }
+
+   const mx_tostadas = (req, res) => {
+    res.render('americas/mx-tostadas', { title: 'Tostadas cuisine' });
+   }
+
+    const mx_chilaquiles = (req, res) => {
+    res.render('americas/mx-chilaquiles', { title: 'Chilaquiles cuisine' });
+   }
+
+   const uruguan_cuisines = (req, res) => {
+    res.render('americas/ur-cuisines', { title: 'Uruguan cuisine' });
+   }
+
+   const ur_asado = (req, res) => {
+    res.render('americas/ur-asado', { title: 'Asado cuisine' });
+   }
+   
+   const ur_chaja = (req, res) => {
+    res.render('americas/ur-chaja', { title: 'Chaja cuisine' });
+   }
+   
+   const ur_revuelto = (req, res) => {
+    res.render('americas/ur_revuelto', { title: 'Revuelto gramaja' });
+   }
+
+   const asian_countries = (req, res) => {
+    res.render('asian/php-sinigang', { title: 'Lechon dish' });
    }
 
    const philippines_cuisines = (req, res) => {
     res.render('asian/philippines-cuisines', { title: 'Philippines cuisines' });
-   
-   }
+    }
  
    const php_lechon = (req, res) => {
     res.render('asian/php-lechon', { title: 'Lechon dish' });
-   
    }
+
+   const php_sinigang = (req, res) => {
+    res.render('asian/php-sinigang', { title: 'Lechon dish' });
+   }
+
+   /*const african_countries = (req, res) => {
+    res.render('african/countries', { title: 'African cuisines' });
+   } */
+    
+   const tanzanian_cuisines = (req, res) => {
+    res.render('african/tz-cuisines', { title: 'Tanzanian cuisines' });
+   } 
    
 
 
@@ -35,5 +117,25 @@ const index = (req, res) => {
     index,
     asian_countries,
     philippines_cuisines,
-    php_lechon
+    php_lechon,
+    php_sinigang,
+    european_countries,
+    spanish_cuisines,
+    esp_gazpaco,
+    esp_paella,
+    esp_gambas,
+    mexican_cuisines,
+    mx_pozole,
+    mx_tostadas,
+    mx_chilaquiles,
+    uruguan_cuisines,
+    ur_asado,
+    ur_chaja,
+    fr_cuisines,
+    fr_monsieur,
+    fr_bouillabaisse,
+    fr_lorraine,
+    ur_revuelto,
+    itl_cuisines
+    
   }
--- routes/primaryRoutes.js
+++ routes/primaryRoutes.js
@@ -4,9 +4,63 @@ const primaryController = require('../controllers/primaryController');
 const router = express.Router();
 
 router.get('/', primaryController.index);
+
+// Africa's routes start here
+//router.get('/africa', primaryController.african_countries);
+//router.get('/tanzanian/cuisines', primaryController.tanzanian_cuisines);
+//router.get('/spanish/cuisines/gazpaco', primaryController.esp_gazpaco);
+//router.get('/spanish/cuisines/paella', primaryController.esp_paella);
+//router.get('/spanish/cuisines/gambas', primaryController.esp_gambas);
+// Africa's routes start here
+
+// Europe routes start here
+router.get('/europe', primaryController.european_countries);
+router.get('/spanish/cuisines', primaryController.spanish_cuisines);
+router.get('/spanish/cuisines/gazpaco', primaryController.esp_gazpaco);
+router.get('/spanish/cuisines/paella', primaryController.esp_paella);
+router.get('/spanish/cuisines/gambas', primaryController.esp_gambas);
+
+router.get('/french/cuisines', primaryController.fr_cuisines);
+router.get('/french/cuisines/bouillabaisse', primaryController.fr_bouillabaisse);
+router.get('/french/cuisines/monsieur', primaryController.fr_monsieur);
+router.get('/french/cuisines/lorraine', primaryController.fr_lorraine);
+
+
+router.get('/itlalian/cuisines', primaryController.itl_cuisines);
+
+/*
+router.get('/italian/cuisines', primaryController.fr_cuisines);
+router.get('/french/cuisines/bouillabaisse', primaryController.fr_bouillabaisse);
+router.get('/french/cuisines/monsieur', primaryController.fr_monsieur);
+router.get('/french/cuisines/lorraine', primaryController.fr_lorraine); */
+// Europe routes ends here
+
+//America's routes starts here
+//router.get('/americas', primaryController.american_countries);
+router.get('/mexican/cuisines', primaryController.mexican_cuisines);
+router.get('/mexican/cuisines/pozole', primaryController.mx_pozole);
+router.get('/mexican/cuisines/tostadas', primaryController.mx_tostadas);
+router.get('/mexican/cuisines/chilaquile', primaryController.mx_chilaquiles);
+
+router.get('/uruguan/cuisines', primaryController.uruguan_cuisines);
+router.get('/uruguan/cuisines/asado', primaryController.ur_asado);
+router.get('/uruguan/cuisines/chaja', primaryController.ur_chaja);
+router.get('/uruguan/cuisines/revuelto', primaryController.ur_revuelto);
+
+
+
+
+
+
+
+// America's routes start here
+
+
 router.get('/asian', primaryController.asian_countries);
 router.get('/philippines/cuisines', primaryController.philippines_cuisines);
 router.get('/philippines/cuisine/lechon', primaryController.php_lechon);
+router.get('/philippines/cuisine/sinigang', primaryController.php_sinigang);
+
 
 
 module.exports = router;
